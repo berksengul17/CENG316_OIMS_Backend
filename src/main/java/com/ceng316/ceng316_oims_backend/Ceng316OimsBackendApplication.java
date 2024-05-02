9d02dafe59d16beabe1d82cc8af8cad8d4687b69
berk-sengul <berk.sengul@davon.com.tr>
2024-05-02 21:07:02 +0300

    init

---
+++ src/main/java/com/ceng316/ceng316_oims_backend/Ceng316OimsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ceng316.ceng316_oims_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ceng316OimsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ceng316OimsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ceng316/ceng316_oims_backend/Ceng316OimsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceng316.ceng316_oims_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ceng316OimsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
