bced7ae36c431ebe8c0e54404eafad72b2b8e341
emreerol0 <emreerol.erol0@gmail.com>
2024-05-18 16:55:01 +0300

    iztech user new constructors

--- src/main/java/com/ceng316/ceng316_oims_backend/IztechUser/IztechUser.java
+++ src/main/java/com/ceng316/ceng316_oims_backend/IztechUser/IztechUser.java
@@ -3,13 +3,17 @@ package com.ceng316.ceng316_oims_backend.IztechUser;
 import com.ceng316.ceng316_oims_backend.Feedback.IztechUserFeedback.IztechUserFeedback;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import java.util.List;
+import java.util.Objects;
 
 
-@Data
+@Getter
+@Setter
+@ToString
+
 @Entity
 // TODO niye no args constructor istiyor öğren
 @NoArgsConstructor
@@ -24,19 +28,51 @@ public class IztechUser{
     private Role role;
     @OneToMany(mappedBy = "iztech_user", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonIgnore
+    @ToString.Exclude
     private List<IztechUserFeedback> feedbacks;
+    private String identityNumber;
+    private Long schoolId;
+    private Integer grade;
+    private String contactNumber;
 
-    public IztechUser(Long id, String fullName, String email, Role role) {
-        this.id = id;
-        this.fullName = fullName;
-        this.email = email;
-        this.role = role;
-    }
 
-    public IztechUser(String fullName, String email, String password, Role role) {
+    public IztechUser(String fullName, String email, String password,
+                      Role role,
+                      String identityNumber, String contactNumber) {
         this.fullName = fullName;
         this.email = email;
         this.password = password;
         this.role = role;
+//        this.feedbacks = feedbacks;
+        this.identityNumber = identityNumber;
+        this.contactNumber = contactNumber;
+    }
+
+    public IztechUser(String fullName, String email, String password,
+                      Role role, List<IztechUserFeedback> feedbacks,
+                      String identityNumber, Long schoolId, Integer grade,
+                      String contactNumber) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+//        this.feedbacks = feedbacks;
+        this.identityNumber = identityNumber;
+        this.schoolId = schoolId;
+        this.grade = grade;
+        this.contactNumber = contactNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        IztechUser that = (IztechUser) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
