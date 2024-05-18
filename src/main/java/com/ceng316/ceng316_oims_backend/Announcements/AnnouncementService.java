2e23e37a7f6c178c5bd727036ee4c0510ebaeada
berksengul17 <berksengul17@gmail.com>
2024-05-18 17:01:10 +0300

    add update announcement

--- src/main/java/com/ceng316/ceng316_oims_backend/Announcements/AnnouncementService.java
+++ src/main/java/com/ceng316/ceng316_oims_backend/Announcements/AnnouncementService.java
@@ -8,6 +8,7 @@ import com.ceng316.ceng316_oims_backend.Documents.DocumentRepository;
 import com.ceng316.ceng316_oims_backend.Documents.DocumentStatus;
 import com.ceng316.ceng316_oims_backend.Documents.DocumentType;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @AllArgsConstructor
@@ -26,6 +28,12 @@ public class AnnouncementService {
     @Transactional
     public Announcement createAnnouncement(String title, MultipartFile file,
                                            LocalDate deadline, Long companyId) throws IOException {
+
+        String validationError = isValidAnnouncement(title, file, deadline);
+        if (validationError != null) {
+            throw new IllegalArgumentException(validationError);
+        }
+
         Document document = new Document(
                 file.getBytes(),
                 file.getContentType(),
@@ -41,6 +49,33 @@ public class AnnouncementService {
         return announcementRepository.save(announcement);
     }
 
+    //FIXME çok fazla null kontrolü var
+    @Transactional
+    public Announcement updateAnnouncement(Long announcementId, String title, MultipartFile file, LocalDate deadline) throws IOException {
+        Announcement announcement = announcementRepository.findById((announcementId))
+                .orElseThrow(() -> new IllegalArgumentException("Announcement not found"));
+
+        String validationError = isValidAnnouncement(title, file, deadline);
+        if (validationError != null) {
+            throw new IllegalArgumentException(validationError);
+        }
+
+        if (file != null) {
+            Document document = announcement.getDocument();
+            document.setContent(file.getBytes());
+            document.setContentType(file.getContentType());
+            documentRepository.save(document);
+        }
+        if (title != null) {
+            announcement.setTitle(title);
+        }
+        if (deadline != null) {
+            announcement.setDeadline(deadline);
+        }
+
+        return announcementRepository.save(announcement);
+    }
+
     public Announcement getAnnouncement(String id) {
         return announcementRepository.findById(Long.parseLong(id))
                 .orElseThrow(() -> new IllegalArgumentException("Announcement not found"));
@@ -52,6 +87,18 @@ public class AnnouncementService {
 
     public List<Announcement> getAnnouncementsForCompany(Long id) {
         return announcementRepository.findByCompanyId(id);
+    }
+
+    private String isValidAnnouncement(String title, MultipartFile file, LocalDate deadline) {
+        if ((title != null) && (title.length() < 3 || title.length() > 50)) {
+            return "Title must be between 3 and 50 characters.";
+        } else if (file != null && !Objects.equals(file.getContentType(), "application/pdf")) {
+            return "File must be a PDF.";
+        } else if (deadline != null && deadline.isBefore(LocalDate.now())) {
+            return "Deadline can't be before the current date.";
+        }
+
+        return null;
     }
 
 }
