192805b3e22f5ae84d5a6542152e0a5009019318
berksengul17 <berksengul17@gmail.com>
2024-05-19 13:57:08 +0300

    add eligible student get controller

--- src/main/java/com/ceng316/ceng316_oims_backend/Student/StudentController.java
+++ src/main/java/com/ceng316/ceng316_oims_backend/Student/StudentController.java
@@ -1,11 +1,22 @@
 package com.ceng316.ceng316_oims_backend.Student;
 
+import com.ceng316.ceng316_oims_backend.IztechUser.IztechUser;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@AllArgsConstructor
+import java.util.List;
 
+@RestController
+@RequestMapping("/students")
+@AllArgsConstructor
 public class StudentController {
     private final StudentService studentService;
+
+    @GetMapping("/eligible-students")
+    public ResponseEntity<List<IztechUser>> getEligibleStudents() {
+        return ResponseEntity.ok(studentService.getEligibleStudents());
+    }
 }
