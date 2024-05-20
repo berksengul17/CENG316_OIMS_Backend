2b0bdd9f278fabd0bb8e32ae32ebc88eee28adfd
berksengul17 <berksengul17@gmail.com>
2024-05-20 22:07:22 +0300

    initialize student with company

--- src/main/java/com/ceng316/ceng316_oims_backend/DataInitializer.java
+++ src/main/java/com/ceng316/ceng316_oims_backend/DataInitializer.java
@@ -20,13 +20,6 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        iztechUserRepository.save(new IztechUser("Admin", "admin@admin.com", "123", Role.SYSTEM_ADMIN, "13154178426", "0555 555 55 55"));
-        IztechUser student = new IztechUser("Berk Şengül", "berk@std.iyte.edu.tr", "123", Role.STUDENT, "13154178426", "0555 555 55 55", 1);
-        student.setGrade("3");
-        student.setSchoolId("280201015");
-
-        iztechUserRepository.save(student);
-        iztechUserRepository.save(new IztechUser("Buket Öksüzoğlu", "buket@iyte.edu.tr", "123", Role.SUMMER_PRACTICE_COORDINATOR, "13154178426", "0555 555 55 55"));
         companyRepository.save(new Company("test_approved@company.com", "123", "Test Company Approved", RegistrationStatus.APPROVED));
         companyRepository.save(new Company("test_pending@company.com", "123", "Test Company Pending", RegistrationStatus.PENDING));
         companyRepository.save(new Company("test_pending@company.com    ", "123", "Test Company Pending", RegistrationStatus.PENDING));
@@ -35,6 +28,13 @@ public class DataInitializer implements CommandLineRunner {
         companyRepository.save(new Company("test_pending4@company.com", "123", "Test Company Pending", RegistrationStatus.PENDING));
         companyRepository.save(new Company("test_pending5@company.com", "123", "Test Company Pending", RegistrationStatus.PENDING));
         companyRepository.save(new Company("test_banned@company.com", "123", "Test Company Banned", RegistrationStatus.BANNED));
-        student.setCompany(companyRepository.findByEmail("test_approved@company.com").get());
+        iztechUserRepository.save(new IztechUser("Admin", "admin@admin.com", "123", Role.SYSTEM_ADMIN, "13154178426", "0555 555 55 55"));
+        IztechUser student = new IztechUser("Berk Şengül", "berk@std.iyte.edu.tr", "123", Role.STUDENT, "13154178426", "0555 555 55 55", 1);
+        student.setGrade("3");
+        student.setSchoolId("280201015");
+        student.setCompany(companyRepository.findByEmail("test_approved@company.com").orElse(null));
+
+        iztechUserRepository.save(student);
+        iztechUserRepository.save(new IztechUser("Buket Öksüzoğlu", "buket@iyte.edu.tr", "123", Role.SUMMER_PRACTICE_COORDINATOR, "13154178426", "0555 555 55 55"));
     }
 }
