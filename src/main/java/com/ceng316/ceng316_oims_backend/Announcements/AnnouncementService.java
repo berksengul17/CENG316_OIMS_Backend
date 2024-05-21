8bc2dd78ddc7c361445778b3edf558ecbcdb3707
emreerol0 <emreerol.erol0@gmail.com>
2024-05-21 10:51:27 +0300

    announcementService

--- src/main/java/com/ceng316/ceng316_oims_backend/Announcements/AnnouncementService.java
+++ src/main/java/com/ceng316/ceng316_oims_backend/Announcements/AnnouncementService.java
@@ -2,13 +2,11 @@ package com.ceng316.ceng316_oims_backend.Announcements;
 
 import com.ceng316.ceng316_oims_backend.Company.Company;
 import com.ceng316.ceng316_oims_backend.Company.CompanyRepository;
-import com.ceng316.ceng316_oims_backend.Company.RegistrationStatus;
 import com.ceng316.ceng316_oims_backend.Documents.Document;
 import com.ceng316.ceng316_oims_backend.Documents.DocumentRepository;
 import com.ceng316.ceng316_oims_backend.Documents.DocumentStatus;
 import com.ceng316.ceng316_oims_backend.Documents.DocumentType;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
