df41abc0be9e9b5bfd1cd380c2792c56ee94cba4
emreerol0 <emreerol.erol0@gmail.com>
2024-05-22 05:37:40 +0300

    sadece initializer sanırım

--- src/main/java/com/ceng316/ceng316_oims_backend/DataInitializer.java
+++ src/main/java/com/ceng316/ceng316_oims_backend/DataInitializer.java
@@ -7,7 +7,6 @@ import com.ceng316.ceng316_oims_backend.Documents.Document;
 import com.ceng316.ceng316_oims_backend.Documents.DocumentRepository;
 import com.ceng316.ceng316_oims_backend.Documents.DocumentStatus;
 import com.ceng316.ceng316_oims_backend.Documents.DocumentType;
-import com.ceng316.ceng316_oims_backend.InternshipApplication.InternshipApplication;
 import com.ceng316.ceng316_oims_backend.InternshipApplication.InternshipApplicationRepository;
 import com.ceng316.ceng316_oims_backend.IztechUser.IztechUser;
 import com.ceng316.ceng316_oims_backend.IztechUser.IztechUserRepository;
@@ -46,16 +45,20 @@ public class DataInitializer implements CommandLineRunner {
         companyRepository.save(new Company("test_pending4@company.com", "123", "Test Company Pending", RegistrationStatus.PENDING));
         companyRepository.save(new Company("test_pending5@company.com", "123", "Test Company Pending", RegistrationStatus.PENDING));
 
-        iztechUserRepository.save(new IztechUser("Admin", "admin@admin.com", "123", Role.SYSTEM_ADMIN, "13154178426", "0555 555 55 55"));
-        iztechUserRepository.save(new IztechUser("Department Secretary", "sec@sec.com", "123", Role.DEPARTMENT_SECRETARY, "13154178426", "0555 555 55 55"));
+        iztechUserRepository.save(new IztechUser("Admin", "admin@admin.com", "123", Role.SYSTEM_ADMIN, "58974826485", "0555 555 55 55"));
+        iztechUserRepository.save(new IztechUser("Department Secretary", "sec@sec.com", "123", Role.DEPARTMENT_SECRETARY, "58974826485", "0555 555 55 55"));
 
-        IztechUser student = new IztechUser("Berk Şengül", "berk@std.iyte.edu.tr", "123", Role.STUDENT, "13154178426", "0555 555 55 55", 1);
+        IztechUser student = new IztechUser("Berk Şengül", "berk@std.iyte.edu.tr", "123", Role.STUDENT, "58974826485", "0555 555 55 55", 1);
         student.setGrade("3");
         student.setSchoolId("280201015");
 
+        IztechUser student1 = new IztechUser("Emre Erol", "emre@std.iyte.edu.tr", "123", Role.STUDENT, "58974826485", "0555 555 55 55", 1);
+        student1.setGrade("3");
+        student1.setSchoolId("280201014");
 
         iztechUserRepository.save(student);
-        iztechUserRepository.save(new IztechUser("Buket Öksüzoğlu", "buket@iyte.edu.tr", "123", Role.SUMMER_PRACTICE_COORDINATOR, "13154178426", "0555 555 55 55"));
+        iztechUserRepository.save(student1);
+        iztechUserRepository.save(new IztechUser("Buket Öksüzoğlu", "buket@iyte.edu.tr", "123", Role.SUMMER_PRACTICE_COORDINATOR, "58974826485", "0555 555 55 55"));
 //        internshipApplicationRepository.save(new InternshipApplication(student,
 //                companyRepository.findByEmail("berksengul9a@gmail.com").orElse(null)));
 
